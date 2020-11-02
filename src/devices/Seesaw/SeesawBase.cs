cdbc8700f890ced164e1b2e3110e2cda71ce5ed9
Rich Lander <rlander@microsoft.com>
2020-11-02 13:20:40 -0800

    Update Seesaw project

--- src/devices/Seesaw/SeesawBase.cs
+++ src/devices/Seesaw/SeesawBase.cs
@@ -31,6 +31,7 @@ namespace Iot.Device.Seesaw
         /// will be disposed when the along with the SeeSaw device</param>
         public Seesaw(I2cDevice i2cDevice)
         {
+            I2cDevice = i2cDevice;
             Initialize(i2cDevice);
         }
 
@@ -66,10 +67,7 @@ namespace Iot.Device.Seesaw
         /// </summary>
         protected void Initialize(I2cDevice i2cDevice)
         {
-            I2cDevice = i2cDevice;
-
             SoftwareReset();
-
             DelayHelper.DelayMilliseconds(10, true);
 
             if (ReadByte(SeesawModule.Status, SeesawFunction.StatusHwId) != SessawHardwareId)
@@ -165,7 +163,7 @@ namespace Iot.Device.Seesaw
         public void Dispose()
         {
             I2cDevice?.Dispose();
-            I2cDevice = null;
+            I2cDevice = null!;
         }
     }
 }
